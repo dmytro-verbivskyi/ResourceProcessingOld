7497631bdf78d1fd486e775c73ef95f830d4c137
dverbovskiy <dverbovskiy@gmail.com>
2013-01-29 18:58:00 +0200

    import javax issue about Jersey

---
+++ src/main/java/dverbovskiy/contacts/Facade.java
@@ -0,0 +1,13 @@
+package dverbovskiy.contacts;
+
+/**
+ * Facade for all GET/POST/PUT/DELETE request
+ * User: dverbovskiy
+ */
+
+import javax.ws.rs.Path;
+
+@Path("contacts")
+public class Facade {
+
+}
