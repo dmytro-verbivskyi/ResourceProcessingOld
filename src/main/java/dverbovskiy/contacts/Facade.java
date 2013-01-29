28bf4efd16328bed945f0534c5e7735639b947d8
dverbovskiy <dverbovskiy@gmail.com>
2013-01-29 19:25:28 +0200

    simpliest GET method implemented

--- src/main/java/dverbovskiy/contacts/Facade.java
+++ src/main/java/dverbovskiy/contacts/Facade.java
@@ -5,9 +5,23 @@ package dverbovskiy.contacts;
  * User: dverbovskiy
  */
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
 @Path("contacts")
 public class Facade {
+    @GET
+    @Produces("text/html")
+    public String get(@Context UriInfo ui) {
+        MultivaluedMap<String, String> params = ui.getQueryParameters();
 
+        if (params.containsKey("list")) {
+
+        }
+        return ("get");
+    }
 }
