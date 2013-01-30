113408f47c812dc5e30b9adc42cbd6b995430976
dverbovskiy <dverbovskiy@gmail.com>
2013-01-30 13:55:38 +0200

    restructured util package, there was a wrong name

---
+++ src/main/java/dverbovskiy/util/DebugUtil.java
@@ -0,0 +1,6 @@
+package dverbovskiy.util;
+
+public class DebugUtil {
+    public static String START = "===== start =====";
+    public static String END = "====== end ======";
+}
--- src/main/java/dverbovskiy/util/DebugUtil/DebugUtil.java
+++
@@ -1,4 +0,0 @@
-package dverbovskiy.util;
-
-public class DebugUtil {
-}
