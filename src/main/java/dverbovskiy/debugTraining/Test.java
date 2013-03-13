8a43e838d11a573319c4389962995417fdceffa6
Dima Home <dverbovskiy@gmail.com>
2013-03-13 12:00:06 +0200

    update 05

--- src/main/java/dverbovskiy/debugTraining/Test.java
+++ src/main/java/dverbovskiy/debugTraining/Test.java
@@ -1,32 +1,18 @@
 package dverbovskiy.debugTraining;
 
+import java.util.Hashtable;
+
 public class Test {
-    public int a;   //--1--
-    public Obj o;   //--2--
-
-    public int getB() {
-//        int b;
-//        Obj o;
-//        return o.getX() + b;   //--3--
-        return 1;
-    }
-
     public static void main(String[] args) {
-       /* Test t = new Test();
-        System.out.println(t.a);        //--4--
-        System.out.println(t.o);        //--5--
-        System.out.println(t.getB());   //--6--*/
+        Hashtable ht = new Hashtable();
+        ht.put("1", "2");
+        ht.put("2", "3");
 
-        System.out.println(false^false);
-        System.out.println(false^true );
-        System.out.println( true^false);
-        System.out.println( true^true );
+        if ( ht.contains("1") ) {
+            System.out.print("1");
+        }
+        if ( ht.contains("2") ) {
+            System.out.print("2");
+        }
     }
-}
-
-class Obj {
-    private int x;
-
-    Obj()             { this.x = 1; }
-    public int getX() { return this.x; }
 }
