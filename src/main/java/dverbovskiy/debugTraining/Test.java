4bbfe0b7fd43b3875b742017b9923edab4b2b3ac
Dima Home <dverbovskiy@gmail.com>
2013-03-14 11:10:18 +0200

    update 06

--- src/main/java/dverbovskiy/debugTraining/Test.java
+++ src/main/java/dverbovskiy/debugTraining/Test.java
@@ -1,18 +1,10 @@
 package dverbovskiy.debugTraining;
 
-import java.util.Hashtable;
-
 public class Test {
-    public static void main(String[] args) {
-        Hashtable ht = new Hashtable();
-        ht.put("1", "2");
-        ht.put("2", "3");
-
-        if ( ht.contains("1") ) {
-            System.out.print("1");
-        }
-        if ( ht.contains("2") ) {
-            System.out.print("2");
+    public static void main(String args[]) {
+        int[] array = {3, 4};
+        for (long x : array) {
+            System.out.print(x);
         }
     }
 }
