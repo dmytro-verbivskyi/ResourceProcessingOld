b932906b591f917883292b829c7291c2ba9bef20
Dima Home <dverbovskiy@gmail.com>
2013-03-15 03:09:40 +0200

    update 07

--- src/main/java/dverbovskiy/debugTraining/Test.java
+++ src/main/java/dverbovskiy/debugTraining/Test.java
@@ -1,10 +1,60 @@
 package dverbovskiy.debugTraining;
 
-public class Test {
-    public static void main(String args[]) {
-        int[] array = {3, 4};
-        for (long x : array) {
-            System.out.print(x);
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class Employee {
+    private String name;
+
+    public Employee(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
+
+    public boolean equals(Object obj){
+        Test.count++;
+
+        if (obj == null) {
+            return false;
         }
+        if (obj.getClass() != getClass()){
+            return false;
+        }
+
+        Employee emp = (Employee)obj;
+        if (this.name == emp.name){
+            return true;
+        }
+        return false;
+    }
+
+    public int hashCode(){
+        return name.hashCode();
+    }
+}
+
+public class Test {
+    static int count = 0;
+
+    public static void main(String[] args) {
+
+        Map employees = new HashMap();
+
+        employees.put(new Employee("Joe"), new Integer("1"));
+        employees.put(new Employee("Chandler"), new Integer("2"));
+        employees.put(new Employee("Chandler"), new Integer("2"));
+        employees.put(new Employee("Ross"), new Integer("3"));
+
+        Iterator iterator = employees.keySet().iterator();
+
+        while (iterator. hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(count);
     }
 }
